9b6497260f6c0e910c066d2a40eee165e0026474
nhnajibullah <nurhasan95@gmail.com>
2019-02-20 09:11:37 +0700

    rename load_form function

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ wp_register_script( 'jquery_inputmask_bundle', "https://cdnjs.cloudflare.com/aja
 wp_register_script( 'jquery_inputmask_binding', "https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/bindings/inputmask.binding.min.js");
 wp_register_style( 'kpr_simulation_css', plugins_url( '/css/styles.css', __FILE__ ) );
 
-function load_form()
+function nks_load_form()
 {
     include( plugin_dir_path( __FILE__ ) . 'views/form.php');
 }
@@ -28,4 +28,4 @@ wp_enqueue_script( 'jquery_inputmask_bundle' );
 wp_enqueue_script( 'jquery_inputmask_binding' );
 wp_enqueue_style('kpr_simulation_css');
 
-add_shortcode( 'nadaft_kpr_simulation_form', 'load_form' );
+add_shortcode( 'nadaft_kpr_simulation_form', 'nks_load_form' );
